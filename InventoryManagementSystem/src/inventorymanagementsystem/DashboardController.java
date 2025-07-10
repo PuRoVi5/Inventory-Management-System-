26cc2c2a0ac7978f14f3035a90a706d998c1c55c
PuRoVi5 <purovi754@gmail.com>
2025-07-10 21:27:54 +0600

    Add files via upload

---
+++ InventoryManagementSystem/src/inventorymanagementsystem/DashboardController.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package inventorymanagementsystem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author PuRoVi
+ */
+public class DashboardController implements Initializable {
+
+    @FXML
+    private Label mainLabel;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void showDashboard(ActionEvent event) {
+    }
+
+    @FXML
+    private void showProducts(ActionEvent event) {
+    }
+
+    @FXML
+    private void showCategories(ActionEvent event) {
+    }
+
+    @FXML
+    private void showSuppliers(ActionEvent event) {
+    }
+
+    @FXML
+    private void showStock(ActionEvent event) {
+    }
+
+    @FXML
+    private void showSales(ActionEvent event) {
+    }
+
+    @FXML
+    private void showReports(ActionEvent event) {
+    }
+
+    @FXML
+    private void showSettings(ActionEvent event) {
+    }
+    
+}
---
+++ InventoryManagementSystem/src/inventorymanagementsystem/LoginController.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package inventorymanagementsystem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author PuRoVi
+ */
+public class LoginController implements Initializable {
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    
+}
